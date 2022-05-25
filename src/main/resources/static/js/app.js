8264cbe3adc0b158d76c5821be3a82693cd301ac
pgrrr <pgrrr119@gmail.com>
2022-05-25 22:57:54 +0900

    .gitignore 추가

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,65 @@
+let inputText = document.querySelectorAll(".input-text");
+let warningMessage = document.querySelectorAll(".message");
+
+if (document.querySelector(".verify-message").value == "fail") {
+   alert("이메일 혹은 비밀번호가 틀렸습니다.");
+}
+
+function emailCheck() {
+   if (
+      inputText[0].value == "" ||
+      !inputText[0].value.includes("@") ||
+      !inputText[0].value.includes(".com")
+   ) {
+      warningMessage[0].style.visibility = "visible";
+      inputText[0].style.borderColor = "red";
+   } else {
+      document.querySelector(".email-form").submit();
+   }
+}
+
+function passwordCheck() {
+   if (
+      inputText[0].value == "" ||
+      !inputText[0].value.includes("@") ||
+      !inputText[0].value.includes(".com")
+   ) {
+      warningMessage[0].style.visibility = "visible";
+      inputText[0].style.borderColor = "red";
+      if (inputText[1].value == "") {
+         warningMessage[1].style.visibility = "visible";
+         inputText[1].style.borderColor = "red";
+      } else {
+         warningMessage[1].style.visibility = "hidden";
+         inputText[1].style.borderColor = "#cacaca";
+      }
+   } else if (inputText[1].value == "") {
+      warningMessage[0].style.visibility = "hidden";
+      inputText[0].style.borderColor = "#cacaca";
+      warningMessage[1].style.visibility = "visible";
+      inputText[1].style.borderColor = "red";
+   } else {
+      document.querySelector(".email-form").submit();
+   }
+}
+
+function nameCheck() {
+   if (inputText[2].value == "") {
+      warningMessage[2].style.visibility = "visible";
+      inputText[2].style.borderColor = "red";
+      if (inputText[1].value == "") {
+         warningMessage[1].style.visibility = "visible";
+         inputText[1].style.borderColor = "red";
+      } else {
+         warningMessage[1].style.visibility = "hidden";
+         inputText[1].style.borderColor = "#cacaca";
+      }
+   } else if (inputText[1].value == "") {
+      warningMessage[2].style.visibility = "hidden";
+      inputText[2].style.borderColor = "#cacaca";
+      warningMessage[1].style.visibility = "visible";
+      inputText[1].style.borderColor = "red";
+   } else {
+      document.querySelector(".email-form").submit();
+   }
+}
