34d3d99a927e56b0b45e6159d8f518e739b2a2b3
pgrrr <pgrrr119@gmail.com>
2022-07-18 19:00:56 +0900

    ⭐ feat: 페이징 기능 세팅

---
+++ src/test/java/nosleepcoders/holeinone/domain/PageHandlerTest.java
@@ -0,0 +1,30 @@
+package nosleepcoders.holeinone.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PageHandlerTest {
+    @Test
+    void 페이지내비테스트1() {
+        // given
+        PageHandler pageHandler = new PageHandler(250, 1);
+        pageHandler.print();
+        // when
+        // then
+        assertEquals(1, pageHandler.getBeginPage());
+        assertEquals(10, pageHandler.getEndPage());
+    }
+
+    @Test
+    void 페이지내비테스트2() {
+        // given
+        PageHandler pageHandler = new PageHandler(250, 11);
+        pageHandler.print();
+        // when
+        // then
+        assertEquals(11, pageHandler.getBeginPage());
+        assertEquals(20, pageHandler.getEndPage());
+    }
+
+}
